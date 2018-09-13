14922cd437d51792235b48e0a880a84d206515d4
AlSteelos35 <asalazar3578@gmail.com>
2018-09-13 11:48:53 -0400

    dbconfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,29 +1,7 @@
 
-const pgp = require('pg-promise')();
+module.exports = process.env.DATABASE_URL || {
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || 'cocktail_db',
 
-let db;
-
-if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-  db = pgp({
-	// custom database name
-	database: 'thundercats_development', 
-	port: 5432,
-	host: 'localhost',
-  });
-} else if (process.env.NODE_ENV === 'production') {
-	// Heroku will add this
-	db = pgp(process.env.DATABASE_URL); 
-}
-
-module.exports = db;
-
-
-
-
-
-// module.exports = process.env.DATABASE_URL || {
-//   host: process.env.DB_HOST || 'localhost',
-//   port: process.env.DB_PORT || 5432,
-//   database: process.env.DB_NAME || 'cocktail_db',
-
-// };
+};
