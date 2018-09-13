69305208eec38e596243e6f0bf42609fc5fa413e
AlSteelos35 <asalazar3578@gmail.com>
2018-09-13 11:17:29 -0400

    fixed the path for heroku

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,29 @@
 
-module.exports = process.env.DATABASE_URL || {
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'cocktail_db',
+const pgp = require('pg-promise')();
 
-};
+let db;
+
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+	// custom database name
+	database: 'thundercats_development', 
+	port: 5432,
+	host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+	// Heroku will add this
+	db = pgp(process.env.DATABASE_URL); 
+}
+
+module.exports = db;
+
+
+
+
+
+// module.exports = process.env.DATABASE_URL || {
+//   host: process.env.DB_HOST || 'localhost',
+//   port: process.env.DB_PORT || 5432,
+//   database: process.env.DB_NAME || 'cocktail_db',
+
+// };
